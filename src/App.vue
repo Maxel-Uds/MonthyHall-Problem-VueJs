cfd80566fcde9a594de6b6ca1b46b10534579e4c
Maxel-Uds <maxellopes32@gmail.com>
2023-01-18 22:30:43 -0300

    Problema de Monty Hall #04

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-    <DoorComponent number="1" :hasGift="false"/>
+    <div class="form">
+        <div v-if="!started">
+            <label for="doorsAmount">Quantidade de portas</label>
+            <input type="text" id="doorsAmount" size="3" v-model.number="doorsAmount">
+        </div>
+
+        <div v-if="!started">
+            <label for="selectedDoor">Qual a porta premiada?</label>
+            <input type="text" id="selectedDoor" size="3" v-model.number="selectedDoor">
+        </div>
+        <button v-if="!started" @click="started = true" :disabled="!selectedDoor || selectedDoor > doorsAmount || selectedDoor < 1">Iniciar</button>
+        <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+
+    <div class="doors" v-if="started">
+        <div v-for="i in doorsAmount" :key="i">
+            <DoorComponent :hasGift="i === selectedDoor" :number="i" @event="openDoorWithGift"/>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +28,22 @@
 
     export default {
         name: 'App',
-        components: { DoorComponent }
+        components: { DoorComponent },
+        data: function () {
+            return {
+                started: false,
+                doorsAmount: 3,
+                selectedDoor: null
+            }
+        },
+        methods: {
+            openDoorWithGift(number, open) {
+                console.log("openDoorWithGift")
+                if(open && this.selectedDoor == number) {
+                    alert('Parabéns, você ganhou o prêmio!');
+                }
+            }
+        }
     }
 </script>
 
@@ -36,5 +69,29 @@
         background-color: #0004;
         padding: 20px;
         margin-bottom: 60px;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 40px;
+    }
+
+    .form, .form input, .form button {
+        margin-bottom: 10px;
+        font-size:2rem;
+    }
+
+    .form input { 
+        margin-left: 15px;
+    }
+
+    .doors {
+        align-self: stretch;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
     }
 </style>
